7bc4cb00a22c4fd47f4ee818bf1e9cdf3870d39b
musmemur <timurkrivoscheev2005@gmail.com>
2025-05-10 15:45:00 +0400

    округлил среднюю оценку релиза

--- frontend/src/features/calculateAverageReleaseRating.ts
+++ frontend/src/features/calculateAverageReleaseRating.ts
@@ -3,5 +3,6 @@ import {RatingModal} from "../entities/RatingModal.ts";
 export function calculateAverageReleaseRating(ratings: RatingModal[]) {
     let sum = 0;
     ratings.forEach(rating => sum += rating.rating);
-    return sum;
+    const averageNum = sum / ratings.length;
+    return averageNum.toFixed(2);
 }
